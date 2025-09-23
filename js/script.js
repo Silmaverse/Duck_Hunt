7e0473c642e322553e7a982331182277857a08cd
silma subah <silmasubah105@gmail.com>
2025-09-23 16:07:44 +0600

    first commit

--- js/script.js
+++ js/script.js
@@ -5,17 +5,28 @@ let duckWidth = 96;
 let duckHeight =93;
 let duckVelocityX =5;
 let duckVelocityY =5;
-
 let gameWidth =window.screen.width;
 let gameHeight = window.screen.height*3/4 ;
 
+let score=0;
+
+// music add
+
+let scoresound =new Audio("../music/dog-score.mp3");
+let duck_flap_sound =new Audio("../music/duck-flap.mp3");
+let duch_quak_sound = new Audio("../music/duck-quack.mp3");
+let duck_shot_sound = new Audio("../music/duck-shot.mp3");
+
 window.onload =function(){
     addDucks();
-    setInterval(moveDucks ,1000/60);
+    setInterval(moveDucks ,1000/50);
 }
 
 function addDucks(){
+  
+  
     ducks=[];
+    duckCount =Math.floor(Math.random()*2)+1;
     for(let i =0 ; i<duckCount ;i++){
         let duckImageName =duckImages[Math.floor(Math.random()*2)];
 
@@ -26,6 +37,27 @@ function addDucks(){
         duckImage.height =duckHeight;
         duckImage.draggable =false;
         duckImage.style.position ="absolute";
+        duckImage.onclick =function(){
+            duck_shot_sound
+            duck_shot_sound.play();
+            score +=1;
+            document.getElementById("score").innerHTML = `Score : ${score}`;
+            document.body.removeChild(this);
+            let remainigducks =[]
+            for(let i=0; i<ducks.length ;i++){
+                if(ducks[i].image != this){
+                    remainigducks.push(ducks[i]);
+                }
+            }
+
+            ducks =remainigducks;
+            if(ducks.length ==0){
+                addDog();
+                
+            }
+
+        }
+
         document.body.append(duckImage);
 
 
@@ -45,6 +77,7 @@ function addDucks(){
         }
 
         ducks.push(duck);
+        duch_quak_sound.play();
     }
 
 }
@@ -58,18 +91,64 @@ function moveDucks(){
         if(duck.x <0 || duck.x +duckWidth > gameWidth){
 
             duck.x -=duck.velocityX;
+
+            duck.velocityX*=-1;
+
+            if(duck.velocityX <0){
+                duck.image.src=duckImages[0] ; //left
+            } else{
+                 duck.image.src=duckImages[1] ; //right
+            }
         }
+
          duck.y +=duck.velocityY;
+
         if(duck.y <0 || duck.y + duckHeight >gameHeight){
 
             duck.y -=duck.velocityY;
+
+            duck.velocityY*=-1 ;
         }
 
 
 
         duck.image.style.left =String(duck.x) + "px";
         duck.image.style.top =String(duck.y) + "px";
+        duck_flap_sound.play();
     }
+}
+
+
+function addDog(){
+    let dogImage =document.createElement("img");
+
+    if(duckCount == 1){
+        dogImage.src ="../images/dog-duck1.png";
+        dogImage.width =172;
+    }
+    else{
+        dogImage.src ="../images/dog-duck2.png";
+        dogImage.width=224;
+        
+    }
+    
+    dogImage.height =152;
+    dogImage.draggable = false;
+    
+    dogImage.style.position ="fixed";
+    dogImage.style.left ="50%";
+    dogImage.style.transform ="translate(-50%)"
+    dogImage.style.bottom ="0px";
+    document.body.appendChild(dogImage);
+
+    scoresound.play();
+
+    setTimeout(()=>{
+        document.body.removeChild(dogImage);
+         addDucks();
+    },1000)
+    
+
 }
 
 function randomPosition(limit){
